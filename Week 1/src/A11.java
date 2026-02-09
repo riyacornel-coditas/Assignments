9702292ba1a8e13e6119b21e41ff67dbd11fec93
riyacornel-coditas <riya.cornel@coditas.com>
2026-02-09 10:19:08 +0530

    First commit

---
+++ Week 1/src/A11.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class A11 {
+    public static void main(String[] args)
+    {
+        int n=6;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+
+            for(int k=1;k<=2*(n-i);k++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int k=1;k<=2;k++)
+        {
+            for(int i=1;i<=n;i++)
+            {
+                System.out.print("*");
+            }
+            for(int i=1;i<=n;i++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=n-1;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+
+            }
+            for(int k=1;k<=2*(n-i);k++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
