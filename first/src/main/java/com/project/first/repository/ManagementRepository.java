f23b4c1d593bca08336303bb8ea3325c30787096
riyacornel-coditas <riya.cornel@coditas.com>
2026-05-05 16:58:59 +0530

    changes made

--- first/src/main/java/com/project/first/repository/ManagementRepository.java
+++
@@ -1,6 +0,0 @@
-package com.project.first.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ManagementRepository extends JpaRepository<Management, Long> {
-}
